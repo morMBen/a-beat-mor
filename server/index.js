33f79b667884ed8a5d11b0907c0fbb12cf33ab83
mor <mor6686@gmail.com>
2021-05-16 14:01:44 +0300

    a-beat-mor_0.2

--- server/index.js
+++ server/index.js
@@ -15,15 +15,15 @@ app.use(express.json({ limit: "5mb" }))
 app.use(cors())
 app.use('/api', route);
 
-app.use((req, res) => {
-    res.status(404).send('Error: Something went wrong... try again')
-})
-
+// app.use((req, res) => {
+//     res.status(404).send('Error: Something went wrong... try again')
+// })
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(PORT, () => {
     console.log('listening to port ' + PORT)
