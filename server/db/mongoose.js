a33a4468eb79a8af8fceec5b6c242ebc262de634
mor <mor6686@gmail.com>
2021-05-16 14:08:54 +0300

    a-beat-mor_0.4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,9 +6,9 @@ const pass = process.env.MONGO_PASSWORD
 //↓↓↓↓↓↓ Local address ↓↓↓↓↓↓
 // mongoose.connect('mongodb://127.0.0.1:27017/beat-mor',
 
-mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0.sibae.mongodb.net/${'myFirstDatabase'}?retryWrites=true&w=majority`,
+// mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0.sibae.mongodb.net/${'myFirstDatabase'}?retryWrites=true&w=majority`,
 
-    // mongoose.connect('mongodb+srv://mordi:FvrfzqluBJ6Ioo1Z@cluster0.sibae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://mordi:FvrfzqluBJ6Ioo1Z@cluster0.sibae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {
         useNewUrlParser: true,
         useCreateIndex: true,
